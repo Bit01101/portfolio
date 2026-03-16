c0ec3cd706d509ae4e2a8ef4203ee79e78c83f49
Bit <bit01101@yandex.ru>
2026-03-17 01:26:17 +0500

    Configure project for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Bit01101.github.io",
-  base: "/mio_portfolio/",
+  base: "/portfolio",
   publicDir: "public",
   server: {
     host: "0.0.0.0",
